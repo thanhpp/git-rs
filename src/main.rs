184029cd7cac77fdb49eda9fccc4a2cfc7936042
thanhpp <thanhphanphu18@gmail.com>
2023-03-27 18:11:33 +0700

    feat: ✨ cat-file
    Signed-off-by: thanhpp <thanhphanphu18@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 use std::env;
 #[allow(unused_imports)]
 use std::fs;
+use std::io::Read;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -9,13 +10,50 @@ fn main() {
 
     // Uncomment this block to pass the first stage
     let args: Vec<String> = env::args().collect();
-    if args[1] == "init" {
-        fs::create_dir(".git").unwrap();
-        fs::create_dir(".git/objects").unwrap();
-        fs::create_dir(".git/refs").unwrap();
-        fs::write(".git/HEAD", "ref: refs/heads/master\n").unwrap();
-        println!("Initialized git directory")
-    } else {
-        println!("unknown command: {}", args[1])
+    match args[1].as_str() {
+        "init" => {
+            fs::create_dir(".git").unwrap();
+            fs::create_dir(".git/objects").unwrap();
+            fs::create_dir(".git/refs").unwrap();
+            fs::write(".git/HEAD", "ref: refs/heads/master\n").unwrap();
+            println!("Initialized git directory");
+        }
+        "cat-file" => cat_file(&args),
+        _ => {
+            println!("unknown command: {}", args[1])
+        }
+    }
+}
+
+fn cat_file(args: &Vec<String>) {
+    if args[2].ne("-p") {
+        return;
+    }
+
+    let hash = args[3].clone();
+    if hash.len() < 2 {
+        return;
+    }
+
+    let hash_prefix = hash[..2].as_bytes();
+    let hash_prefix = std::str::from_utf8(hash_prefix).unwrap();
+
+    for entry in fs::read_dir(".git/objects").unwrap() {
+        let path = entry.unwrap().path();
+        let basename = path.file_name().unwrap();
+
+        if !basename.eq(hash_prefix) {
+            continue;
+        }
+
+        // read file
+        for f_entry in fs::read_dir(path).unwrap() {
+            let file_content = fs::read(f_entry.unwrap().path()).unwrap();
+            let mut z = flate2::read::ZlibDecoder::new(&file_content[..]);
+            let mut s = String::new();
+            z.read_to_string(&mut s).unwrap();
+            println!("{}", s);
+            return;
+        }
     }
 }
